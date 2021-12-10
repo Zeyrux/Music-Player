1a2c0b74a5706d71e64e9edb6ef508ab24c97423
Zeyrux <tappetheo@gmail.com>
2021-12-10 21:48:33 +0100

    change style and removed simikolons

--- static/scripts/audio controller.js
+++ static/scripts/audio controller.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", function() {
-    let attributeMax = document.createAttribute("max");
-    attributeMax.value = Math.round(audio.duration);
-    inputRangeSongProgress.setAttributeNode(attributeMax);
+    let attributeMax = document.createAttribute("max")
+    attributeMax.value = Math.round(audio.duration)
+    inputRangeSongProgress.setAttributeNode(attributeMax)
 });
 
 window.addEventListener("resize", function() {
@@ -10,28 +10,28 @@ window.addEventListener("resize", function() {
 
 inputButtonPlayPause.addEventListener("click", function() {
     if (audio.paused) {
-        audio.play();
-        inputButtonPlayPause.innerHTML = "Pause";
+        audio.play()
+        inputButtonPlayPause.innerHTML = "Pause"
     } else {
-        audio.pause();
-        inputButtonPlayPause.innerHTML = "Play";
+        audio.pause()
+        inputButtonPlayPause.innerHTML = "Play"
     }
 });
 
 inputRangeVolume.addEventListener("input", function() {
-    audio.volume = this.value / 100;
+    audio.volume = this.value / 100
     inputTextVolume.setAttribute("value", this.value)
 });
 
 inputRangeSongProgress.addEventListener("input", function() {
-    audio.currentTime = this.value;
-    paragraphTime.innerHTML = this.value + " / " + Math.round(audio.duration);
+    audio.currentTime = this.value
+    paragraphTime.innerHTML = this.value + " / " + Math.round(audio.duration)
 });
 
 setInterval(function() {
-    inputRangeSongProgress.value = Math.round(audio.currentTime);
-    paragraphTime.innerHTML = Math.round(audio.currentTime) + " / " + Math.round(audio.duration);
+    inputRangeSongProgress.value = Math.round(audio.currentTime)
+    paragraphTime.innerHTML = Math.round(audio.currentTime) + " / " + Math.round(audio.duration)
     if (audio.currentTime >= audio.duration - 1) {
-        formSkipVolume.submit();
+        formSkipVolume.submit()
     }
-}, 1000);
+}, 1000)
--- static/scripts/definition.js
+++ static/scripts/definition.js
@@ -1,31 +1,31 @@
-var audio = document.getElementById("audio");
-var paragraphSongName = document.getElementById("paragraphSongName");
-var inputButtonPlayPause = document.getElementById("inputButtonPlayPause");
-var formSkipVolume = document.getElementById("formSkipVolume");
-var inputTextVolume = document.getElementById("inputTextVolume");
-var inputButtonSkip = document.getElementById("inputButtonSkip");
-var paragraphTime = document.getElementById("paragraphTime");
-var inputRangeSongProgress = document.getElementById("inputRangeSongProgress");
-var inputRangeVolume = document.getElementById("inputRangeVolume");
-var canvas = document.getElementById('analyser_render');
+var audio = document.getElementById("audio")
+var paragraphSongName = document.getElementById("paragraphSongName")
+var inputButtonPlayPause = document.getElementById("inputButtonPlayPause")
+var formSkipVolume = document.getElementById("formSkipVolume")
+var inputTextVolume = document.getElementById("inputTextVolume")
+var inputButtonSkip = document.getElementById("inputButtonSkip")
+var paragraphTime = document.getElementById("paragraphTime")
+var inputRangeSongProgress = document.getElementById("inputRangeSongProgress")
+var inputRangeVolume = document.getElementById("inputRangeVolume")
+var canvas = document.getElementById('analyser_render')
 
 function resize() {
-    canvas.width = screen.width - 50;
-    canvas.height = screen.height - 225;
-    bars = (screen.width - 50) / 4;
+    canvas.width = screen.width - 40
+    canvas.height = screen.height - 20
+    bars = (screen.width - 50) / 4
 }
 
-var bars;
+var bars
 
-var song = document.currentScript.getAttribute("song");
-song = song.replaceAll("#SPACE#", " ");
-song = song.slice(0, song.length - 1);
-paragraphSongName.innerHTML = song;
+var song = document.currentScript.getAttribute("song")
+song = song.replaceAll("#SPACE#", " ")
+song = song.slice(0, song.length - 1)
+paragraphSongName.innerHTML = song
 
-let volume = document.currentScript.getAttribute("volume");
-audio.volume = volume / 100;
-inputRangeVolume.value = volume;
+let volume = document.currentScript.getAttribute("volume")
+audio.volume = volume / 100
+inputRangeVolume.value = volume
 
-paragraphTime.innerHTML = "0 / " + Math.round(audio.duration);
+paragraphTime.innerHTML = "0 / " + Math.round(audio.duration)
 
 resize();
